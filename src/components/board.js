7cecd8c1cc45d20f26e25643bbfaaf7e40ebabb9
Georg <georgiy.nikitin@gmail.com>
2020-04-14 11:57:41 +0300

    Выделен компонент списка задач

--- src/components/board.js
+++ src/components/board.js
@@ -3,7 +3,6 @@ import {createElement} from "../utils.js";
 const createBoardTemplate = () => {
   return (
     `<section class="board container">
-      <div class="board__tasks"></div>
     </section>`
   );
 };
